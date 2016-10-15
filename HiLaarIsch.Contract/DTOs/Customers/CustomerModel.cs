1202e6f9a7d219da25aedeb789e0b56ee51141a9
QuantumHive <QuantumHive@outlook.com>
2016-10-15 21:22:25 +0200

    customer view reflects database properties

--- HiLaarIsch.Contract/DTOs/Customers/CustomerModel.cs
+++ HiLaarIsch.Contract/DTOs/Customers/CustomerModel.cs
@@ -6,9 +6,15 @@ namespace HiLaarIsch.Contract.DTOs
     public class CustomerModel
     {
         public Guid Id { get; set; }
+
         public string Firstname { get; set; }
         public string Surname { get; set; }
-        public string Email { get; set; }
         public GroupLevel GroupLevel { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string EmergencyNumber { get; set; }
+        public string Address { get; set; }
+
+        public string Email { get; set; }
     }
 }

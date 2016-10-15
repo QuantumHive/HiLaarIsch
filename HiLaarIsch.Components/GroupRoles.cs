099b54bc3a30eab6e86ec6f590aedb7ee3045371
QuantumHive <QuantumHive@outlook.com>
2016-10-15 20:26:04 +0200

    components assembly for shared architecture

---
+++ HiLaarIsch.Components/GroupRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace HiLaarIsch.Components
+{
+    public enum GroupRole
+    {
+        //0 reserved
+        Shetlander = 0,
+        Children = 1,
+        Adults = 2,
+    }
+}
---
+++ HiLaarIsch.Components/Roles.enum.cs
@@ -0,0 +1,9 @@
+﻿namespace HiLaarIsch.Components
+{
+    public enum Role
+    {
+        //0 reserved
+        Admin = 1,
+        Customer = 2,
+    }
+}

3ad5fa1790299177a38efca394ace2226582c34f
QuantumHive <QuantumHive@outlook.com>
2016-10-15 19:44:33 +0200

    database update: KISS user, and customer has additional columns as discussed

--- SqlScripts/Migrations.sql
+++ SqlScripts/Migrations.sql
@@ -1,10 +1,8 @@
 CREATE TABLE [dbo].[Users] (
     [Id] uniqueidentifier NOT NULL,
-    [Username] nvarchar(100) NOT NULL,
     [PasswordHash] nvarchar(100) NULL,
     [Email] nvarchar(100) NULL,
     [EmailConfirmed] bit NOT NULL,
-    [SecurityStamp] uniqueidentifier NULL,
     [Role] tinyint NOT NULL,
     CONSTRAINT [PK_UserId] PRIMARY KEY ([Id])
 )
@@ -14,17 +12,21 @@ CREATE TABLE [dbo].[Customers] (
     [Id] uniqueidentifier NOT NULL,
     [Surname] nvarchar(100) NOT NULL,
     [Firstname] nvarchar(100) NOT NULL,
-    [DateOfBirth] date NOT NULL,
-    [Gender] bit NOT NULL,
-    [UserId] uniqueidentifier NOT NULL,
+    [Address] nvarchar(200) NOT NULL,
+    [PhoneNumber] nvarchar(100) NOT NULL,
+    [EmergencyNumber] nvarchar(100) NOT NULL,
+    [GroupLevel] tinyint NOT NULL,
+    [FK_UserId] uniqueidentifier UNIQUE NOT NULL,
     CONSTRAINT [PK_CustomerId] PRIMARY KEY ([Id])
 )
 GO
 
-ALTER TABLE [dbo].[Customers] WITH CHECK ADD CONSTRAINT [FK_Customers_Users] FOREIGN KEY([UserId]) REFERENCES [dbo].[Users] ([Id])
+ALTER TABLE [dbo].[Customers] WITH CHECK ADD CONSTRAINT [FK_Customers_Users] FOREIGN KEY([FK_UserId]) REFERENCES [dbo].[Users] ([Id])
 GO
 ALTER TABLE [dbo].[Customers] CHECK CONSTRAINT [FK_Customers_Users]
 GO
 
-INSERT INTO [dbo].[Users] VALUES ('1f88d96d-ae68-4a25-9e92-2e27bbaf750f', 'admin', 'AGtWGj6m/ICb16LNfGINdZTceQH9xdE9l9QbFKhuv0DaoDI2Ja/AeVCCK8BZey3I9g==', 'admin@managearnhem.nl', 1, NULL, 1)
+INSERT INTO [dbo].[Users] VALUES ('1f88d96d-ae68-4a25-9e92-2e27bbaf750f', 'AGtWGj6m/ICb16LNfGINdZTceQH9xdE9l9QbFKhuv0DaoDI2Ja/AeVCCK8BZey3I9g==', 'admin@managearnhem.nl', 1, 1)
 GO
+
+

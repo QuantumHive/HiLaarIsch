de6f25995fe4e4054a91f96d2d8a7194ed696b00
QuantumHive <QuantumHive@outlook.com>
2016-11-14 20:31:20 +0100

    typo fix

--- SqlScripts/Migrations.sql
+++ SqlScripts/Migrations.sql
@@ -26,7 +26,7 @@ GO
 ALTER TABLE [dbo].[Customers] CHECK CONSTRAINT [FK_Customers_Users]
 GO
 
-INSERT INTO [dbo].[Users] VALUES ('1f88d96d-ae68-4a25-9e92-2e27bbaf750f', 'AGtWGj6m/ICb16LNfGINdZTceQH9xdE9l9QbFKhuv0DaoDI2Ja/AeVCCK8BZey3I9g==', 'admin@managearnhem.nl', 1, 1)
+INSERT INTO [dbo].[Users] VALUES ('1f88d96d-ae68-4a25-9e92-2e27bbaf750f', 'AGtWGj6m/ICb16LNfGINdZTceQH9xdE9l9QbFKhuv0DaoDI2Ja/AeVCCK8BZey3I9g==', 'admin@manegearnhem.nl', 1, 1)
 GO
 
 

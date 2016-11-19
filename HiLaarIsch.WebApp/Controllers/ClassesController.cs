f62d417ec0de20663b095eaba0ce0ec88ec6659c
QuantumHive <QuantumHive@outlook.com>
2016-11-19 18:42:23 +0100

    -classes controller with empty index -some minor refactoring

---
+++ HiLaarIsch.WebApp/Controllers/ClassesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using HiLaarIsch.Components;
+using HiLaarIsch.Filters;
+using QuantumHive.Core;
+
+namespace HiLaarIsch.Controllers
+{
+    [Authorize]
+    [AuthorizeRole(Role.Admin)]
+    [RoutePrefix("classes")]
+    public class ClassesController : Controller
+    {
+        private readonly IQueryProcessor queryProcessor;
+
+        public ClassesController(IQueryProcessor queryProcessor)
+        {
+            this.queryProcessor = queryProcessor;
+        }
+
+        [HttpGet, Route]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ HiLaarIsch.WebApp/Extensions/HtmlPartialExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace HiLaarIsch
+{
+    public static class HtmlPartialExtensions
+    {
+        public static MvcHtmlString PartialAddCommand(this HtmlHelper helper, string route)
+        {
+            return helper.Partial(Partials.Commands.Add, route);
+        }
+    }
+}

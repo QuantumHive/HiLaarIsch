d57ecf7bc9004e979f6a5996e244e44ac3c7d19a
QuantumHive <QuantumHive@outlook.com>
2016-12-03 01:31:31 +0100

    version manager to show version

---
+++ HiLaarIsch.WebApp/Helpers/VersionManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+namespace HiLaarIsch
+{
+    public static class VersionManager
+    {
+        public static readonly Version Version = Assembly.GetExecutingAssembly().GetName().Version;
+
+        public static string FriendlyVersion => $"v{VersionManager.Version.Major}.{VersionManager.Version.Minor}.{VersionManager.Version.Build}";
+    }
+}
